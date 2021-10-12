460bef83e45d9b5c1fd09ffb9560c25a2d5dd4f6
Ryan Parker <ryan.parker3@outlook.com>
2021-10-12 13:27:56 -0700

    post-solution(select): created layer ontop of selector w/styles and set select's `opacity: 0`

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,23 +9,68 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <Wrapper value={value} onChange={onChange}>
-      {children}
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        {/* @ts-ignore - TS doesn't like CSS vars */}
+        <IconWrapper style={{ '--size': '24px' }}>
+          <Icon id='chevron-down' strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.select`
-  width: ${(props) => props.value?.length * 17}px;
-  height: 43px;
-  border-radius: 8px;
-  padding: 12px 16px;
-  background: ${COLORS.transparentGray15};
-  color: ${COLORS.gray700};
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  isolation: isolate;
 
   &:focus {
     color: ${COLORS.black};
   }
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* (Safari): Without this declaration, the select will be locked to a smaller height. */
+  -webkit-appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  padding: 12px 16px;
+  padding-right: 52px;
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
 `;
 
 export default Select;
